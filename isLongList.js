af3f9d49f7572d4eb1b240e0114c0e8eee6c196a
wmckeever <wil.mckeever@gmail.com>
2022-09-28 08:39:17 -0400

    Correction made for resubmission

--- isLongList.js
+++ isLongList.js
@@ -4,10 +4,10 @@ const prompt = require('prompt-sync')({sigint: true});
 
 let list1 = JSON.parse(prompt("Please enter an array:  ")); 
 
-if (list1.length > 10){
-    console.log("Your length is at least 10 >> true")
+if (list1.length >= 10){
+    console.log("Your length is at least 10 items >> true")
 }else{
-    console.log("Your length is not at least 10 >> false");
+    console.log("Your length is not at least 10 items >> false");
 }
 
 
